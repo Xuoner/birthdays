090cc48cd791a31f78059e2c7cb26f463dc297c7
Paul Renoux <74930749+Xuoner@users.noreply.github.com>
2025-02-26 13:40:33 +0100

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -66,13 +66,13 @@ FUNNY_COMMENTS = [
 ]
 
 def load_ideas():
-    return pd.read_csv(CSV_FILE)
+    return pd.read_csv(CSV_FILE, sep = "\t")
 
 def save_idea(idea):
     df = load_ideas()
     new_entry = pd.DataFrame([[idea]], columns=["Idée"])
     df = pd.concat([df, new_entry], ignore_index=True)
-    csv_content = df.to_csv(index=False)
+    csv_content = df.to_csv(index=False, sep="\t")
     # Récupérer le SHA du fichier (nécessaire pour l'update)
     headers = {"Authorization": f"token {GITHUB_TOKEN}"}
     get_response = requests.get(API_URL, headers=headers)
