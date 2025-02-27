abf1cecead5ab9fd8a2c0d797a85705c4ee47e59
Paul Renoux <74930749+Xuoner@users.noreply.github.com>
2025-02-27 16:59:29 +0100

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -279,7 +279,7 @@ if page == "Boîte à idées 💡":
     # Affichage des idées enregistrées sous forme de tableau
     st.subheader("📜 Idées précédentes")
     df = load_ideas()
-    print(df)
+    print(df.columns)
     if df.empty:
         st.info("Aucune idée n'a encore été soumise.")
     else:
